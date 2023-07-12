1bb7f4a3493f348577d2f119c31d14e18104b311
amalvarghese593 <amalvarghese593@gmail.com>
2023-07-12 16:29:45 +0530

    Initialize project using Create React App

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      zIndex: {
+        1111: "1111",
+      },
+    },
+  },
+  plugins: [],
+};
